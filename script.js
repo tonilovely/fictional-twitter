b870493df080eabb9afe9f60ce5e3e88c5e16413
tonilovely <tonilovely420@gmail.com>
2024-06-09 17:42:52 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Add any interactivity here
-// For example, you can add event listeners to the tweet button to open a tweet modal
+document.addEventListener('DOMContentLoaded', () => {
+    const moreMenu = document.querySelector('.more-menu');
+    const moreButton = document.querySelector('.sidebar ul li a[href="#"]');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const tweetButton = document.querySelector('.tweet-button');
-    tweetButton.addEventListener('click', function() {
-        alert('Tweet button clicked!');
-        // Add functionality to open tweet modal here
+    moreButton.addEventListener('click', () => {
+        moreMenu.classList.toggle('show');
     });
 });
